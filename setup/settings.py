03d688751c51089d71e4c3e04ad2e74675290edd
Bilal Ahmed <47696353+bilalahmedkhatri@users.noreply.github.com>
2026-04-25 23:40:30 +0500

    added new auth testing url

--- setup/settings.py
+++ setup/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False')
 
-ALLOWED_HOSTS = [os.getenv('DJANGO_ALLOWED_HOSTS'), 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [os.getenv('DJANGO_ALLOWED_HOSTS'), 'localhost', '127.0.0.1', 'store-dj-rhel-bilalahmedkhatrioutlookcoms-projects.vercel.app',]
 
 
 # Application definition
